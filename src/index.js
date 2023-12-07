4c367e817c7ae3b0973302e5b94ebf732abeada3
Favio Valero <favio102@protonmail.com>
2023-12-07 04:24:44 -0800

    styles updated

--- src/index.js
+++ src/index.js
@@ -3,11 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
-const rootStyle = {
-  backgroundColor: 'black', // Change to your desired background color
-  height: '100vh', // Set height to fill the entire viewport if needed
-  /* Add more styles as needed */
-};
-
-ReactDOM.render(<div style={rootStyle}><App /></div>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
